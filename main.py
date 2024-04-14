aca504a003c60e969b4231e711153f3d87895b0d
Sheepo915 <csymen@gmail.com>
2024-04-14 18:32:28 +0800

    Minor update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib.controller import Controller
 from lib.utils import setup_logger
 
 def main():
-    logger = setup_logger("sensor_log", "./log", "sensor.log")
+    logger = setup_logger("sensor_log", "./logs", "sensor.log")
 
     sensor_manager = SensorManager(logger=logger, print_to_console=True, use_legacy_dht=True)
     controller = Controller(mqtt_host=config.MQTT_BROKER_ADDRESS, mqtt_port=config.MQTT_BROKER_PORT)
